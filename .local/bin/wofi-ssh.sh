83e4e84703610a21931e0b74c29c26ea2e891aae
jwno-git <161667580+jwno-git@users.noreply.github.com>
2025-05-15 16:21:53 -0600

    Update wofi-ssh.sh

--- .local/bin/wofi-ssh.sh
+++ .local/bin/wofi-ssh.sh
@@ -3,7 +3,6 @@
 CHOICE=$(wofi --dmenu --prompt -y 5 -x 1220 "SSH to:" < ~/.config/wofi/ssh_hosts)
 
 if [ -n "$CHOICE" ]; then
-    terminator -e "sh -c '$CHOICE'" &
+    foot --app-id=ssh --title="SSH: $CHOICE" sh -c "$CHOICE" &
     disown
 fi
-
