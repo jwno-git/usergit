48bd17d7d59c1df2d204e0d5d2fc9bbb7e5a38a8
jwno-git <161667580+jwno-git@users.noreply.github.com>
2025-05-17 10:57:59 -0600

    Update volume-notify.sh

--- .local/scripts/volume-notify.sh
+++ .local/scripts/volume-notify.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+##!/bin/bash
 
 # Change the volume
 pactl set-sink-volume @DEFAULT_SINK@ "$1"
@@ -8,4 +8,3 @@ VOLUME=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\d+%' | head -1)
 
 # Send a notification
 dunstify -r 91190 -u low "Volume: $VOLUME"
-
